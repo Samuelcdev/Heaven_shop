84c31c76cd0889685702a2d6bebeea6ead98b8d8
Samuel Useche Chaparro <samuuseche01@gmail.com>
2025-10-08 22:00:43 -0500

    Initial commit: build project

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import sequelize from "./config/database.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor Corriendo exitosamente");
+});
+
+const connectDB = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Conexion con la DB exitosa");
+    } catch (error) {
+        console.log("Algo fallo en la conexion a la DB", error);
+    }
+};
+
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
