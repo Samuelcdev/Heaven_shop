84ddf23e057702d0d3256471c305acb2b80b3ac1
Samuelcdev <samuuseche01@gmail.com>
2025-10-10 14:46:38 -0500

    feat(backend): Add verifyToken and permissions middlewares

---
+++ backend/src/middlewares/permit.js
@@ -0,0 +1,17 @@
+export const permit = (...allowedRoles) => {
+    return (req, res, next) => {
+        const user = req.user;
+
+        if (!user) {
+            return res.status(401).json({ error: "Not authenticated" });
+        }
+
+        if (!allowedRoles.includes(user.role)) {
+            return res
+                .status(403)
+                .json({ error: "Forbidden: insuficient permissions" });
+        }
+
+        next();
+    };
+};
