ac39cd5556eee4bbb6e9ea5f924ce0a673100e83
Samuelcdev <samuuseche01@gmail.com>
2025-12-23 01:27:02 -0500

    feat(backend): Add service and controller to movement in inventory

--- backend/src/controllers/inventory.controller.js
+++ backend/src/controllers/inventory.controller.js
@@ -2,8 +2,19 @@ import * as inventoryHistoryService from "../services/inventoryHistory.service.j
 
 export const getMonthlyHistory = async (_req, res, next) => {
     try {
-        const historyData = await inventoryHistoryService.getMonthlyInventoryHistory();
+        const historyData =
+            await inventoryHistoryService.getMonthlyInventoryHistory();
         return res.status(200).json(historyData);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const createInventoryMovement = async (req, res, next) => {
+    try {
+        const movement =
+            await inventoryHistoryService.registerInventoryMovement(req.body);
+        return res.status(201).json(movement);
     } catch (err) {
         next(err);
     }
--- backend/src/services/inventoryHistory.service.js
+++ backend/src/services/inventoryHistory.service.js
@@ -1,20 +1,50 @@
 import { fn, col, literal } from "sequelize";
 import InventoryHistory from "../models/InventoryHistory.js";
+import Variant from "../models/Variant.js";
 
 export const getMonthlyInventoryHistory = async () => {
     const reportMonthly = await InventoryHistory.findAll({
         attributes: [
             [fn("DATE_FORMAT", col("created_at"), "%Y-%m"), "month"],
-            [fn("SUM", col("value")), "total_value"],
-            [fn("SUM", col("quantity")), "total_quantity"],
+            [
+                fn(
+                    "SUM",
+                    literal(
+                        `CASE WHEN type = 'in' THEN value WHEN type = 'out' THEN -value END`
+                    )
+                ),
+                "total_value",
+            ],
         ],
-        where: {
-            type: "in",
-        },
         group: [literal("month")],
         order: [[literal("month"), "ASC"]],
         raw: true,
     });
 
     return reportMonthly;
+};
+
+export const registerInventoryMovement = async ({
+    id_variant,
+    quantity,
+    type,
+}) => {
+    const variant = await Variant.findByPk(id_variant);
+
+    if (!variant) {
+        const err = new Error("Variant not found");
+        err.status = 404;
+        throw err;
+    }
+
+    const value = quantity * variant.price_variant;
+
+    const history = await InventoryHistory.create({
+        id_variant,
+        quantity,
+        value,
+        type,
+    });
+
+    return history;
 };
